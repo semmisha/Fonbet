4973f148b7c642445ef26176986e189b9e89e5ff
Sementsov mikhail <semmisha@yandex.ru>
2022-05-15 22:40:52 +0300

    fonbet 15.05.2022 Lenovo

--- db/postgresql/dbEvent.go
+++ db/postgresql/dbEvent.go
@@ -3,12 +3,10 @@ package Postgres
 import (
 	fonstruct "Fonbet/json"
 	"context"
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/sirupsen/logrus"
-	"time"
-
 	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"log"
 )
 
@@ -28,7 +26,7 @@ func Events(fonbet *fonstruct.FonbetEvents, db *pgxpool.Pool, logger *logrus.Log
 			}
 
 		}
-		var fontime = time.Now().Add(7 * time.Hour).Unix()
+		//var fontime = time.Now().Add(7 * time.Hour).Unix()
 		if sum == 0 && fonbet.Events[i].Team1Id != 0 && fonbet.Events[i].Team2Id != 0 && fonbet.Events[i].Level == 1 {
 			query := fmt.Sprintf("INSERT INTO events (id, sportid, team1id, team2id, team1, team2, starttime) VALUES ($1, $2, $3,$4,$5,$6,$7)")
 			_, err := db.Exec(context.Background(), query, fonbet.Events[i].Id, fonbet.Events[i].SportId, fonbet.Events[i].Team1Id, fonbet.Events[i].Team2Id, fonbet.Events[i].Team1, fonbet.Events[i].Team2, fonbet.Events[i].StartTime)
@@ -39,7 +37,7 @@ func Events(fonbet *fonstruct.FonbetEvents, db *pgxpool.Pool, logger *logrus.Log
 			}
 			for _, b := range fonbet.CustomFactors {
 				for _, c := range b.Factors {
-					if fonbet.Events[i].StartTime >= fontime && b.E == fonbet.Events[i].Id && (c.F == 921 || c.F == 922 || c.F == 923) {
+					if b.E == fonbet.Events[i].Id && (c.F == 921 || c.F == 922 || c.F == 923) {
 						query := fmt.Sprintf(`UPDATE events  set "%v" = %v where id = $1`, c.F, c.V)
 						_, err := db.Exec(context.Background(), query, fonbet.Events[i].Id)
 						if err != nil {
--- db/postgresql/dbResult.go
+++ db/postgresql/dbResult.go
@@ -27,21 +27,20 @@ func Result(fonbet *fonstruct.FonbetResult, db *pgxpool.Pool, logger *logrus.Log
 			}
 		}
 
-		if sum == 0 && strings.ContainsAny(fonbet.Events[i].Name, "-–") {
+		if strings.ContainsAny(fonbet.Events[i].Name, "-––") && fonbet.Events[i].Status == 3 {
 
 			strarray := strings.Split(fonbet.Events[i].Score, " ")
 			resultarray := strings.Split(strarray[0], ":")
 
 			if len(resultarray) >= 2 && resultarray[0] != " " && resultarray[1] != " " {
-				_, err = db.Exec(context.Background(), "INSERT INTO results (stringname, starttime, score,team1,team2) VALUES ($1, $2, $3, $4, $5)", fonbet.Events[i].Name, fonbet.Events[i].StartTime, fonbet.Events[i].Score, resultarray[0], resultarray[1])
-				j := &count
-				*j++
-				if err != nil {
-					fmt.Println(err)
+				if sum == 0 {
+					_, err = db.Exec(context.Background(), "INSERT INTO results (stringname, starttime, score,team1,team2) VALUES ($1, $2, $3, $4, $5)", fonbet.Events[i].Name, fonbet.Events[i].StartTime, fonbet.Events[i].Score, resultarray[0], resultarray[1])
+					j := &count
+					*j++
+					if err != nil {
+						fmt.Println(err)
+					}
 				}
-			} else {
-				logger.Errorf("result array <2 symbols, panic. err:%v", err)
-				return err
 
 			}
 		}
