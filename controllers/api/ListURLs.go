19a1cdcc2a45a51b7729c44d54566cb2ccaf95b1
Sementsov mikhail <semmisha@yandex.ru>
2022-07-12 11:00:58 +0300

    Commit Mac 12.07.2022 Fixed ListOfURL's

--- controllers/api/ListURLs.go
+++ controllers/api/ListURLs.go
@@ -36,24 +36,19 @@ func (fonbet *ListURLStruct) JsonToStruct(url string, logger *logrus.Logger) err
 		if err != nil {
 			logger.Errorf("Cant JsonToStruct URL: %v  error: %v", url, err)
 		}
-
 		body, err := io.ReadAll(request.Body)
 		if err != nil {
 			logger.Errorf("Cant ReadALL URL: %v  error: %v", url, err)
 
 		}
 		err = json.Unmarshal(body, &fonbet)
+		request.Body.Close()
 		if err != nil {
 			logger.Errorf("Cant Unmarshall URL: %v  error: %v", url, err)
-		}
-		err = request.Body.Close()
-		if err != nil {
-			logger.Errorf("Unable to close body URL: %v  error: %v", url, err)
-		}
-		if err == nil {
+		} else {
 			return nil
 		}
 		time.Sleep(3 * time.Minute)
 	}
-	return errors.New("Failed to parse list of URLs")
+	return errors.New("Unable to retrieve list of URL's")
 }
