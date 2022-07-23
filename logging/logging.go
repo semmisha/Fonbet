107a04567756fa402f40bd4092634eec2d804112
Sementsov mikhail <semmisha@yandex.ru>
2022-07-23 03:22:06 +0300

    Commit Mac 23.07.2022 Fixed Logs Json

--- logging/logging.go
+++ logging/logging.go
@@ -13,23 +13,17 @@ func Logger() (logger *logrus.Logger) {
 	logger = logrus.New()
 	logger.ReportCaller = true
 
-	logger.SetFormatter(&logrus.TextFormatter{
-		ForceColors:               true,
-		DisableColors:             false,
-		EnvironmentOverrideColors: false,
-		DisableTimestamp:          false,
-		FullTimestamp:             false,
-		TimestampFormat:           "",
-		DisableSorting:            false,
-		SortingFunc:               nil,
-		DisableLevelTruncation:    false,
-		QuoteEmptyFields:          false,
-		FieldMap:                  nil,
+	logger.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat:   "",
+		DisableTimestamp:  false,
+		DisableHTMLEscape: false,
+		DataKey:           "",
+		FieldMap:          nil,
 		CallerPrettyfier: func(frm *runtime.Frame) (function string, file string) {
 			file = path.Base(frm.File)
 			return frm.Function, fmt.Sprintf("file:%v , line:%v", file, frm.Line)
-
 		},
+		PrettyPrint: false,
 	})
 	file, err := os.OpenFile("/app/logging/logs.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
 	if err != nil {
