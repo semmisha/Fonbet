b1a583e424f58f4979bb7854dd3501e2e3fa308c
Sementsov mikhail <semmisha@yandex.ru>
2022-08-28 22:33:35 +0300

    Added telegram hook

--- logging/logging.go
+++ logging/logging.go
@@ -2,7 +2,9 @@ package logging
 
 import (
 	"fmt"
+	"github.com/semmisha/ClientAPI"
 	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/writer"
 	"io"
 	"os"
 	"path"
@@ -12,24 +14,41 @@ import (
 func Logger() (logger *logrus.Logger) {
 	logger = logrus.New()
 	logger.ReportCaller = true
-
-	logger.SetFormatter(&logrus.JSONFormatter{
-		TimestampFormat:   "",
-		DisableTimestamp:  false,
-		DisableHTMLEscape: false,
-		DataKey:           "",
-		FieldMap:          nil,
+	logger.SetFormatter(&logrus.TextFormatter{
+		ForceColors:               false,
+		DisableColors:             true,
+		EnvironmentOverrideColors: false,
+		DisableTimestamp:          true,
+		FullTimestamp:             false,
+		TimestampFormat:           "",
+		DisableSorting:            false,
+		SortingFunc:               nil,
+		DisableLevelTruncation:    false,
+		QuoteEmptyFields:          false,
+		FieldMap:                  nil,
 		CallerPrettyfier: func(frm *runtime.Frame) (function string, file string) {
 			file = path.Base(frm.File)
-			return frm.Function, fmt.Sprintf("file:%v , line:%v", file, frm.Line)
+			return fmt.Sprintf("%s", frm.Function), fmt.Sprintf("file:%v , line:%v", file, frm.Line)
+
 		},
-		PrettyPrint: false,
 	})
+	logger.SetOutput(io.Discard)
+
+	tlgWriter := ClientAPI.NewConnection("@test111_111", "http://172.16.14.67:9999/PostMessage")
 	file, err := os.OpenFile("/app/logging/logs.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
 	if err != nil {
-		logger.Errorf("Unable to Create/open file: %v", file.Name())
+		logger.Error("Unable to Create/open file:", file.Name())
 	}
 
-	logger.Out = io.MultiWriter(os.Stdout, file)
-	return
+	logger.AddHook(&writer.Hook{
+		Writer:    os.Stdout,
+		LogLevels: logrus.AllLevels,
+	})
+	logger.AddHook(&writer.Hook{
+		Writer:    tlgWriter,
+		LogLevels: []logrus.Level{logrus.FatalLevel, logrus.PanicLevel, logrus.ErrorLevel},
+	})
+	//logger.Out = io.MultiWriter(os.Stdout, file)
+
+	return logger
 }
